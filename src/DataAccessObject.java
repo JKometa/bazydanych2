01ae6ed9c93356dc6fcc56df1b9f8840abbf53f8
servlok <jakubstrzalek@gmail.com>
2013-06-17 15:32:54 -0700

    dodano klase DataAccessObject

---
+++ src/DataAccessObject.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: servlok
+ * Date: 17.06.13
+ * Time: 15:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataAccessObject {
+    //TODO
+}
